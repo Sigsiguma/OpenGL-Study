495594bc88807c9e816782d6f7b3816f911ea0fc
Siguma <you8jun5@gmail.com>
2017-02-08 18:46:45 +0900

    Initial Commit

---
+++ OpenGL-Study/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGL-Study
+//
+//  Created by  Siguma Rin on 2017/02/08.
+//  Copyright © 2017年  Siguma Rin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
