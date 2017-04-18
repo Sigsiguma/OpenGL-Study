28febb832acead2db311acff6c338a53b5c978fb
Siguma <you8jun5@gmail.com>
2017-04-18 16:26:24 +0900

    イベントの取り出し方法をポーリング方式に修正

--- main.cpp
+++ main.cpp
@@ -5,9 +5,63 @@
 #include <glm/gtx/transform.hpp>
 #include <cstdlib>
 #include <iostream>
+#include <sstream>
+#include <string>
 #include "ProgramObjectCreator.h"
 #include "VBO.h"
 
+double calcFPS(GLFWwindow* window, double theTimeInterval = 1.0, std::string theWindowTitle = "NONE") {
+    // Static values which only get initialised the first time the function runs
+    static double t0Value = glfwGetTime(); // Set the initial time to now
+    static int fpsFrameCount = 0;             // Set the initial FPS frame count to 0
+    static double fps = 0.0;           // Set the initial FPS value to 0.0
+
+    // Get the current time in seconds since the program started (non-static, so executed every time)
+    double currentTime = glfwGetTime();
+
+    // Ensure the time interval between FPS checks is sane (low cap = 0.1s, high-cap = 10.0s)
+    // Negative numbers are invalid, 10 fps checks per second at most, 1 every 10 secs at least.
+    if (theTimeInterval < 0.1) {
+        theTimeInterval = 0.1;
+    }
+    if (theTimeInterval > 10.0) {
+        theTimeInterval = 10.0;
+    }
+
+    // Calculate and display the FPS every specified time interval
+    if ((currentTime - t0Value) > theTimeInterval) {
+        // Calculate the FPS as the number of frames divided by the interval in seconds
+        fps = (double) fpsFrameCount / (currentTime - t0Value);
+
+        // If the user specified a window title to append the FPS value to...
+        if (theWindowTitle != "NONE") {
+            // Convert the fps value into a string using an output stringstream
+            std::ostringstream stream;
+            stream << fps;
+            std::string fpsString = stream.str();
+
+            // Append the FPS value to the window title details
+            theWindowTitle += " | FPS: " + fpsString;
+
+            // Convert the new window title to a c_str and set it
+            const char *pszConstString = theWindowTitle.c_str();
+            glfwSetWindowTitle(window, pszConstString);
+        } else // If the user didn't specify a window to append the FPS to then output the FPS to the console
+        {
+            std::cout << "FPS: " << fps << std::endl;
+        }
+
+        // Reset the FPS frame counter and set the initial time to be now
+        fpsFrameCount = 0;
+        t0Value = glfwGetTime();
+    } else // FPS calculation time interval hasn't elapsed yet? Simply increment the FPS frame counter
+    {
+        fpsFrameCount++;
+    }
+
+    // Return the current FPS - doesn't have to be used if you don't want it!
+    return fps;
+}
 
 int main(void) {
 
@@ -88,27 +142,45 @@ int main(void) {
                                                   100.0f);
     glm::mat4 vp = projectionMatrix * viewMatrix;
 
-    glm::mat4 mMatrix1 = glm::translate(modelMatrix, glm::vec3(1.5, 0.0, 0.0));
-    glm::mat4 mvp1 = vp * mMatrix1;
-
-    glm::mat4 mMatrix2 = glm::translate(modelMatrix, glm::vec3(-1.5, 0.0, 0.0));
-    glm::mat4 mvp2 = vp * mMatrix2;
 
     auto uniLocation = glGetUniformLocation(program, "mvpMatrix");
+
+    int count = 0;
+
+    glm::mat4 mMatrix1, mMatrix2, mMatrix3;
+    glm::mat4 mvp1, mvp2, mvp3;
 
     while (glfwWindowShouldClose(window) == GL_FALSE) {
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBindVertexArray(vao);
+        ++count;
+
+        calcFPS(window, 1.0, "Test:");
+
+        float rad = (count % 360) * M_PI / 180;
+        float x = cos(rad);
+        float y = sin(rad);
+
+        mMatrix1 = glm::translate(modelMatrix, glm::vec3(x, y + 1.0, 0.0));
+        mvp1 = vp * mMatrix1;
         glUniformMatrix4fv(uniLocation, 1, GL_FALSE, &mvp1[0][0]);
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
+        mMatrix2 = glm::translate(modelMatrix, glm::vec3(1.0, -1.0, 0.0));
+        mMatrix2 = glm::rotate(mMatrix2, rad, glm::vec3(0, 1, 0));
+        mvp2 = vp * mMatrix2;
         glUniformMatrix4fv(uniLocation, 1, GL_FALSE, &mvp2[0][0]);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        mMatrix3 = glm::translate(modelMatrix, glm::vec3(-1.0, -1.0, 0.0));
+        mMatrix3 = glm::scale(mMatrix3, glm::vec3(y + 1.0, y + 1.0, 0));
+        mvp3 = vp * mMatrix3;
+        glUniformMatrix4fv(uniLocation, 1, GL_FALSE, &mvp3[0][0]);
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(window);
 
-        glfwWaitEvents();
+        glfwPollEvents();
     }
 
 }
